ab3b46f949ec219871ee547dde374a9806e34128
Prem Patel <patelprem7736@gmail.com>
2023-04-28 11:50:50 +0530

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
-    return (
-        <main>
-            <h1>In God We Trust</h1>
-        </main>
-    );
+  return (
+    <main>
+      <h1>In God We dont' Trust</h1>
+    </main>
+  );
 }
